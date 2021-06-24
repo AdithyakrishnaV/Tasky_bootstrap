7b632c724438511d37f7a70df936498294c59b78
AdithyakrishnaV <adithyakr007@gmail.com>
2021-06-24 21:04:31 +0530

    add delete feature

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = [];
+let globalStore = [];
 
 const generateNewCard = (taskData) => `
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4" >
     <div class="card text-center">
       <div class="card-header d-flex justify-content-end gap-2">
-        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-dumpster-fire"></i></button>
+        <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i>
+        </button>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)" >
+        <i class="fas fa-dumpster-fire" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i>
+        </button>
       </div>
       <img src=${taskData.imageUrl}
       class="card-img-top" alt="img"
@@ -31,9 +34,10 @@ const loadInitialCardData = () =>{
    //convert from string to normal object
   const {cards} = JSON.parse(getCardData);  
 
-  //loop over the arrays of task object to create HTML card, inject it to DOM
+  //loop over the arrays of task object to create HTML card
   cards.map((cardObject) => {
 
+    // inject it to DOM
     taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
      
     //update globalStore
@@ -56,4 +60,25 @@ globalStore.push(taskData);
 
 localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
 
+};
+
+const deleteCard = (event) => {
+  event = window.event;
+  //id
+  const targetID = event.target.id;
+
+  const tagname = event.target.tagName; // BUTTON
+
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);  
+  localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+ // we have updated array of cards
+   
+  //contact parent 
+
+  if(tagname === "BUTTON") {
+     return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }else {
+     return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+    
 };
